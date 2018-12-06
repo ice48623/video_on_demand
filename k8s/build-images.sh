9f966fdb4b2687e3f46e42a759ee9c20aa92275e
Khemmatat Theanvanichpant <khemmatat.thean@gmail.com>
2018-12-06 22:21:32 +0700

    Use docker hub images instead of local repo

--- k8s/build-images.sh
+++ k8s/build-images.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-docker build -t vod-module https://github.com/ice48623/vod_module.git
-docker build -t vod-frontend https://github.com/ice48623/vod_frontend.git
-docker build -t vod-webapi https://github.com/ice48623/vod_webapi.git
-docker build -t vod-converter https://github.com/ice48623/vod_converter.git
-docker build -t vod-like-worker https://github.com/ice48623/vod_like_worker.git
-docker build -t vod-comment-worker https://github.com/ice48623/vod_comment_worker.git
+docker build -t tui95/vod-module https://github.com/ice48623/vod_module.git
+docker push tui95/vod-module
+docker build -t tui95/vod-frontend https://github.com/ice48623/vod_frontend.git
+docker push tui95/vod-frontend
+docker build -t tui95/vod-webapi https://github.com/ice48623/vod_webapi.git
+docker push tui95/vod-webapi
+docker build -t tui95/vod-converter https://github.com/ice48623/vod_converter.git
+docker push tui95/vod-converter
+docker build -t tui95/vod-like-worker https://github.com/ice48623/vod_like_worker.git
+docker push tui95/vod-like-worker
+docker build -t tui95/vod-comment-worker https://github.com/ice48623/vod_comment_worker.git
+docker push tui95/vod-comment-worker
